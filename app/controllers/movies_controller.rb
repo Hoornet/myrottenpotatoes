3850cd71c4ee9905632c59418362908471d31dfe
Hoornet <hoornet@gmail.com>
2013-02-16 17:47:56 +0100

    added new

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,15 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) #poišči film preko ID
     #po defaultu rendiraj v app/views/movies/show.html.haml
   end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
 end
