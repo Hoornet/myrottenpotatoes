0d8b846f3c3ca1de13ed0c493733fc0e50886759
Hoornet <hoornet@gmail.com>
2013-02-16 14:21:38 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '2cbdc0c3d8aa49d48459bc3b4dd7f71379e65bfb16bdb957118f408dae56e1652069a833822f176418d144f799e56b60b7bc614d1d3f1eab4495e6f3ce2b72c2'
